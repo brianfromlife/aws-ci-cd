e29f957ece18954a84ebb7a81bce6f11550a13ce
bbates <bbates@captechventures.com>
2021-05-05 09:49:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (_req, res) => {
-  return res.send("hello");
+  return res.send("hell");
 });
 
 module.exports = { app };
